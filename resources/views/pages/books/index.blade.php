0e8a17a52922c6bf817cbb42184c131af98c2f9e
= <=>
2023-10-20 04:38:26 +0200

    ADD books table

--- resources/views/pages/books/index.blade.php
+++ resources/views/pages/books/index.blade.php
@@ -11,47 +11,7 @@
         </a>
 
         @if ($books->count() > 0)
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>{{ __('app.books.field.title') }}</th>
-                        <th>{{ __('app.books.field.author') }}</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($books as $book)
-                        <tr>
-                            <td>{{ $book->id }}</td>
-                            <td>{{ $book->title }}</td>
-                            <td>{{ $book->author }}</td>
-                            <td width="220px">
-                                <div class="d-flex justify-content-around">
-                                    <div class="mr-2">
-                                        <a
-                                            class="btn btn-primary"
-                                            href="{{ route('books.edit', $book->id) }}"
-                                        >
-                                            {{ __('app.books.button.update') }}
-                                        </a>
-                                    </div>
-                                    <form
-                                        action="{{ route('books.destroy', $book->id) }}"
-                                        method="POST"
-                                    >
-                                        <button class="btn btn-danger">
-                                            {{ __('app.books.button.delete') }}
-                                        </button>
-                                        @csrf
-                                        <input name="_method" type="hidden" value="DELETE">
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            @include('pages.books.partials.books-table')
         @else
             <div class="alert alert-info text-center">
                 {{ __('app.books.message.no-books') }}
